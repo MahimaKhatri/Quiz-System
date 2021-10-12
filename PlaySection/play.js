1536d333b79b9c20cdfaea051e6f858e92abeca3
Mahima Khatri <66864785+Mahi8@users.noreply.github.com>
2021-10-12 21:35:06 +0530

    Questions added

---
+++ PlaySection/play.js
@@ -0,0 +1,121 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'How many time zones are there in Russia?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '11',
+        choice4: '17',
+        answer: 3,
+    },
+    {
+        question:
+            "The tallest building in the world is located in which city?",
+        choice1: "Dubai",
+        choice2: "New York",
+        choice3: "Shanghai",
+        choice4: "None of the above",
+        answer: 1,
+    },
+    {
+        question: "How many keys does a classic piano have?",
+        choice1: "20",
+        choice2: "118",
+        choice3: "88",
+        choice4: "133",
+        answer: 3,
+    },
+    {
+        question: "Which famous American pop band was originally called ‘Kara’s Flowers’?",
+        choice1: "Maroon 5",
+        choice2: "Pink",
+        choice3: "The Animals",
+        choice4: "BTS",
+        answer: 1,
+    },
+    {
+        question: "Which football team is known as ‘The Red Devils’?",
+        choice1: "Manchester United",
+        choice2: "FC Barcelona",
+        choice3: "Arsenal F.C.",
+        choice4: "Juventus F.C.",
+        answer: 1,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 5
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/Endpage/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
